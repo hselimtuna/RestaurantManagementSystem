2ea2f46ab1e38206890d51b1f3bc16ad1daaf56c
Halil Selim TUNA <hselimtuna@gmail.com>
2021-11-08 17:47:00 +0300

    Connection to Forgot page ensured

--- Front/loginscreen.py
+++ Front/loginscreen.py
@@ -1,13 +1,10 @@
 import time
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSlot, QCoreApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication
-
 from Front.mainmenu import Ui_main_menu
-
+from Front.forgotmypassword import Ui_forgot_password_page
 import sys
-
 
 class Ui_RMS(object):
     def __init__(self):
@@ -18,18 +15,17 @@ class Ui_RMS(object):
         self.loginpage_label = QtWidgets.QLabel(RMS)
         self.forgot_link = QtWidgets.QCommandLinkButton(RMS)
         self.login_button.clicked.connect(self.window2)
-
-
-
-
+        self.forgot_link.clicked.connect(self.window3)
 
     def window2(self):
         self.w = Ui_main_menu()
         self.setup = self.w.setupUi(self.w)
         self.w.main_menu.show()
 
-
-
+    def window3(self):
+        self.x = Ui_forgot_password_page()
+        self.setupf = self.x.setupUi(self.x)
+        self.x.forgot_password_page.show()
 
     def setupUi(self, RMS):
         RMS.setObjectName("RMS")
@@ -80,6 +76,7 @@ class Ui_RMS(object):
         font.setPointSize(12)
         self.loginpage_label.setFont(font)
         self.loginpage_label.setObjectName("loginpage_label")
+
     def retranslateUi(self, RMS):
         _translate = QtCore.QCoreApplication.translate
         RMS.setWindowTitle(_translate("RMS", "RMS"))
@@ -88,9 +85,6 @@ class Ui_RMS(object):
         self.password_label.setText(_translate("RMS", "Password"))
         self.forgot_link.setText(_translate("RMS", "Forgot my password"))
         self.loginpage_label.setText(_translate("RMS", "LOGIN PAGE"))
-
-
-
 
 if __name__ == "__main__":
     import sys
